cfe9a221847eed4d6264eeb842c1c2a9bb8be008
awwalker <awwalker3@gmail.com>
2016-04-21 16:35:38 -0400

    massive cleanup

--- state/static/js/state_inequality.js
+++ state/static/js/state_inequality.js
@@ -1,7 +1,6 @@
 /* TO DO */
     // 1. Prettify coloring
-    // 2. Add bar chart for comparisons
-    // 3. Add recoloring into map...for when new metrics/years are selected
+    // 2. Add line chart for comparisons
     // 4. Code clean up
 /* CURRENTLY */
     // Selecting State can link with selecting Date/Metric
@@ -11,39 +10,34 @@
 var width  = 960,
     height = 500,
     centered;
-
-// Zoom - implement zoom on map
-function zoom() {
-    g.transition()
-    .duration(750)
-    .attr("transform", "translate(" + d3.event.translate + ")scale(" +
-    d3.event.scale + ")");
-};
+var years = []; // Years included in dropdown/data.
+for(i = 1917; i < 2013; i++){
+    years.push(i);
+}
+// For choropleth coloring.
 var color = d3.scale.threshold()
                     .domain([ 1, 3, 10, 15, 20, 25, 30, 35, 40])
                     .range(["#F0F8FF", "#BBFFFF", "#BCD2EE", "#6D9BF1",
                             "#436EEE", "#2E37FE", "#3232CC", "00008B", "000033"]);
-// Add map layers
+// Add map layers.
 var projection = d3.geo.albersUsa().scale(1000).translate([width / 2, height / 2]);
-var path = d3.geo.path().projection(projection);
-var map = d3.select("#map").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .call(d3.behavior.zoom().scaleExtent([1,8]).on("zoom", zoom));
-// Base layer to interact with
-var america = map.append("g")
-                .attr("id", "states");
+var path       = d3.geo.path().projection(projection);
+var map        = d3.select("#map").append("svg")
+                   .attr("width", width)
+                   .attr("height", height)
+                   .append("g");
+// Base layer to interact with.
+var america  = map.append("g")
+                  .attr("id", "states");
+var g        = map.append("g");
+var dataMap  = d3.map();
 
-var g = map.append("g");
-var data_map = d3.map();
-var exclusions = ["Puerto Rico", "Virgin Islands of the United States", "states-borders"];
 // Read in data.
 queue()
     .defer(d3.json, "../static/json/us-named.json") // map.
     .defer(d3.csv, "../static/data/state_inequality.csv", // data.
                 // store each data row as [state, date] in map
-                function(d) { data_map.set([d.State, new Date(+d.Year, 0, 1)], d); })
+                function(d) { dataMap.set([d.State, new Date(+d.Year, 0, 1)], d); })
     .await(ready);
 /* SET UP MAIN MAP*/
 function ready(error, us) {
@@ -63,56 +57,23 @@ function ready(error, us) {
         .attr("id", "states-borders")
         .attr("d", path);
 
-    var years = []; // Years included in dropdown/data.
-    for(i=1917; i < 2013; i++){
-        years.push(i);
-    }
     var metrics = ["Share of Total Wealth Earned by Richest 10%",
-                    "Total Income", "Adjusted Gross Income",
-                    "Consumer Price Index 2014",
                     "Share of Total Wealth Earned by Richest 5%",
                     "Share of Total Wealth Earned by Richest 1%",
                     "Share of Total Wealth Earned by Richest 0.5%",
                     "Share of Total Wealth Earned by Richest 0.1%",
-                    "Share of Total Wealth Earned by Richest 0.01%"];
-    /* SET UP DROPDOWN MENUS */
-    // Allow options to show up.
-    var selectedMetric = "Share of Total Wealth Earned by Richest 10%";
-    var selectedYear = "1917";
-    function colorMap(){
-        var currentState = d3.select(this).attr("id");
-        if(currentState != "Puerto Rico" &&
-            currentState !="Virgin Islands of the United States" &&
-            currentState != "states-borders") {
-            return color(data_map.get(
-                [currentState, new Date(+selectedYear, 0, 1)])[selectedMetric] );
-        }
-    };
-    var state = america.selectAll("path");
-    state.attr("fill", colorMap);
+                    "Share of Total Wealth Earned by Richest 0.01%",
+                    "Total Income", "Adjusted Gross Income",
+                    "Consumer Price Index 2014"
+                    ];
+    /* SET UP DROPDOWN MENU */
     var checkOption = function(e) {
-        if(e === yrselect) {
+        if(e === metricselect) {
             return d3.select(this).attr("selected", "selected");
         }
     };
-    // Year dropdown.
-    // Allow menu to change current year.
-    function yearChanged() {
-        selectedYear = d3.event.target.value; // Change year.
-        state.attr("fill", colorMap); // Recolor.
-    }
-    var yearDrop = d3.select("#year_drop")
-                        .append("select")
-                        .attr("id", "yrselect")
-                        .on("change", yearChanged);
-        yearDrop.selectAll("option")
-                        .data(d3.values(years))
-                        .enter()
-                        .append("option")
-                        .attr("value", function(d) {return d;})
-                        .text(function(d) {return d;});
-        yearDrop.selectAll("option").each(checkOption);
-    // Metric dropdown.
+    // Allow options to show up.
+    var selectedMetric = "Share of Total Wealth Earned by Richest 10%";
     // Allow menu to change current metric.
     function metricChanged() {
         selectedMetric = d3.event.target.value; // Change metric.
@@ -129,17 +90,32 @@ function ready(error, us) {
                     .attr("value", function(d) {return d;})
                     .text(function(d) {return d;});
         metricDrop.selectAll("option").each(checkOption);
+
+    /* CHOROPLETH COLORING */
+    function colorMap(){
+        var currentState = d3.select(this).attr("id");
+        if(currentState != "Puerto Rico" &&
+            currentState !="Virgin Islands of the United States" &&
+            currentState != "states-borders") {
+            return color(dataMap.get(
+                // Color map based on most recent data to start.
+                [currentState, new Date(2012, 0, 1)])[selectedMetric]);
+        }
+    };
+    var state = america.selectAll("path");
+    state.attr("fill", colorMap);
+
     /* SUPPORT STATE CLICKING */
     // On click will return data from given year and reshifts bar graphs
     // to center around that state and year
     var selectedState = "United States"; // global state
     state.on("click", function() {
         selectedState = d3.select(this).attr("id")
-        console.log(data_map.get([selectedState, new Date(+selectedYear, 0, 1)])
-                    [selectedMetric]);
-        console.log(color(data_map.get([selectedState, new Date(+selectedYear, 0, 1)])
-                    [selectedMetric]));
+        years.forEach(function(d){
+            console.log(dataMap.get([selectedState, new Date(+d, 0, 1)])[selectedMetric]);
+            console.log(color(dataMap.get([selectedState, new Date(+d, 0, 1)])[selectedMetric]));
+        });
     });
 };
 
-d3.select(self.frameElement).style("height", height + "px");
+// d3.select(self.frameElement).style("height", height + "px");
