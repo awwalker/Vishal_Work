e0438e033eb1b51afd2f3e295a606435591cac3d
awwalker <awwalker3@gmail.com>
2016-04-27 23:56:35 -0400

    graph almost done

--- state/static/js/state_inequality.js
+++ state/static/js/state_inequality.js
@@ -11,6 +11,7 @@ var width  = 960,
     height = 500,
     centered;
 var margin = {top: 20, right: 80, bottom: 30, left: 50};
+// Set up dropdown.
 // Variables for selection.
 var metrics = ["Share of Total Wealth Earned by Richest 10%",
                 "Share of Total Wealth Earned by Richest 5%",
@@ -21,16 +22,23 @@ var metrics = ["Share of Total Wealth Earned by Richest 10%",
                 "Total Income", "Adjusted Gross Income",
                 "Consumer Price Index 2014"
                 ];
+var checkOption = function(e) {
+     if(e === metricselect) {
+         return d3.select(this).attr("selected", "selected");
+     }
+ };
 var menu = d3.select("#metric_drop")
+             .append("select")
+             .attr("id", "metricselect")
              .on("change", change);
-    menu.selectAll("option")
-        .data(d3.values(metrics))
-        .enter()
-        .append("option")
-        .attr("value", function(d) {return d;})
-        .text(function(d) {return d;});
+menu.selectAll("option")
+    .data(d3.values(metrics))
+    .enter()
+    .append("option")
+    .attr("value", function(d) {return d;})
+    .text(function(d) {return d;});
+menu.selectAll("option").each(checkOption);
 
-var altKey;
 // For choropleth coloring.
 var color = d3.scale.threshold()
                     .domain([ 1, 3, 10, 15, 20, 25, 30, 35, 40])
@@ -47,16 +55,32 @@ var map        = d3.select("#map").append("svg")
 var america  = map.append("g")
                   .attr("id", "states");
 var g        = map.append("g");
-var dataMap  = d3.map();
 
-// Read in data.
-queue()
-    .defer(d3.json, "../static/json/us-named.json") // map.
-    .defer(d3.csv, "../static/data/state_inequality.csv", // data.
-                function(d) {
-                    data = d;
-                })
-    .await(ready);
+// To be used later.
+var altKey,
+    states;
+// Set up graph.
+var line = d3.svg.line()
+             .interpolate("basis")
+             .x(function(d) {return x(d.year); })
+             .y(function(d) {return y(d.stat); });
+
+var chart = d3.select("#rate_chart").append("svg")
+              .attr("width", w + margin.left + margin.right)
+              .attr("height", h + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Set up axises.
+var xAxis = d3.svg.axis()
+              .scale(x)
+              .orient("bottom")
+            svg.append("svg:g")
+               .attr("class", "x axis");
+var yAxis = d3.svg.axis()
+              .scale(y)
+              .orient("left")
+            svg.append("svg:g")
+               .attr("class", "y axis");
 
 function change() {
     clearTimeout(timeout);
@@ -65,47 +89,51 @@ function change() {
       .each(redraw);
 }
 
+d3.csv("../static/data/state_inequality_nest.csv", function(csv) {
+    csv.forEach(function(d) {
+        d.Year = new Date(+d.Year, 0, 1)
+    })
+    states = csv;
+    redraw();
+});
+
 function redraw() {
-    
-}
-/* SET UP MAIN MAP */
-function ready(error, us) {
-    if(error) throw error;
-    // console.log(dataMap["California"][new Date(2012, 0, 1)]);
-    d3.select("#Loading").style("display", "none") // Hide loading message.
-    america.selectAll("path")
-        .data(topojson.feature(us, us.objects["states"]).features)
-        .enter()
-        .append("path")
-        .attr("id", function(d) {return d.properties.name;})
-        .attr("d", path);
-
-    america.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a,b) {
-            return a!==b;
-        }))
-        .attr("id", "states-borders")
-        .attr("d", path);
-
-    /* CHOROPLETH COLORING */
-    function colorMap(){
-        var currentState = d3.select(this).attr("id");
-        if(currentState != "Puerto Rico" &&
-            currentState !="Virgin Islands of the United States" &&
-            currentState != "states-borders") {
-            return color( dataMap[currentState][new Date(2012, 0, 1)][selectedMetric])
-            };
-    }
-    var state = america.selectAll("path");
-    state.attr("fill", colorMap);
-
-    /* SUPPORT STATE CLICKING */
-    var selectedState = "United States"; // global state
-    state.on("click", function() {
-        selectedState = d3.select(this).attr("id")
-        console.log(dataMap[selectedState]);
+    var nested = d3.nest()
+                   .key(function(d) { return d.Label; })
+                   .map(states)
+    console.log(nested);
+    var series = menu.property("value");
+    console.log(series);
+    // Retrieve only data for selected metric.
+    var data = nested[series];
+    // keys are each individual state & DC.
+    var keyring = d3.keys(data[0]).filter(function(key) {
+        return (key !== "Label 2" && key !== "Year" && key !== "Label");
     });
+    console.log(keyring);
+    // get the year and related statistics and map them to each state separately.
+    var transpose = keyring.map(function(name) {
+        return {
+            name: name,
+            values: data.map(function(d) {
+                return {year: d.Year, stat: + d[name]};
+            })
+        };
+    });
+    console.log(transpose);
+    // scale x / y domains according to current values.
+    x.domain([
+        d3.min(transpose, function(c) { return d3.min(c.values, function(v) { return v.year; }); });
+        d3.max(transpose, function(c) { return d3.max(c.values, function(v) { return v.year; }); })
+    ]);
+    y.domain([
+        d3.min(transpose, function(c) { return d3.min(c.values, function(v) { return v.year; }); });
+        d3.max(transpose, function(c) { return d3.max(c.values, function(v) { return v. year; }); })
+    ]);
+}
 
-};
-
-// d3.select(self.frameElement).style("height", height + "px");
+// automatically change value after a few seconds
+var timeout = setTimeout(function() {
+    menu.property("value", metrics[0]).node().focus();
+    change();
+}, 7000);
